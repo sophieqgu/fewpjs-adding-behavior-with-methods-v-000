3af496edd02c6c3726d67c0adb8d094ddc9e843c
Sophie Gu <sophieqgu@gmail.com>
2021-01-27 05:09:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+class Cat {
+  speak: (name) => `${name} says meow!`;
+}
+
+class Dog {
+  speak: (name) => `${name} says woof!`;
+}
+
+class Bird {
+  speak: (name) => `${name} says squawk!`;
+}
